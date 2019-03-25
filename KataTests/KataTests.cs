294789d706a02921aa533e01dcc18d4d20104b5d
Laurent Michel <laurent.michel@softozor.ch>
2019-03-25 10:20:40 +0100

    add my solution to the Counting Duplicates kata

--- KataTests/KataTests.cs
+++ KataTests/KataTests.cs
@@ -12,5 +12,16 @@ namespace KataTests
       Assert.AreEqual(3, ShortestWord.FindShort("bitcoin take over the world maybe who knows perhaps"));
       Assert.AreEqual(3, ShortestWord.FindShort("turns out random test cases are easier than writing out basic ones"));
     }
+
+    [TestMethod]
+    public void DuplicateCountTests()
+    {
+      Assert.AreEqual(0, CountingDuplicates.DuplicateCount(""));
+      Assert.AreEqual(0, CountingDuplicates.DuplicateCount("abcde"));
+      Assert.AreEqual(2, CountingDuplicates.DuplicateCount("aabbcde"));
+      Assert.AreEqual(2, CountingDuplicates.DuplicateCount("aabBcde"), "should ignore case");
+      Assert.AreEqual(1, CountingDuplicates.DuplicateCount("Indivisibility"));
+      Assert.AreEqual(2, CountingDuplicates.DuplicateCount("Indivisibilities"), "characters may not be adjacent");
+    }
   }
 }
---
+++ Katas/CountingDuplicates.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace Katas
+{
+  public class CountingDuplicates
+  {
+    /*
+     * Write a function that will return the count of distinct
+     * case-insensitive alphabetic characters and numeric digits
+     * that occur more than once in the input string. The input
+     * string can be assumed to contain only alphabets (both
+     * uppercase and lowercase) and numeric digits.
+     */
+    public static int DuplicateCount(string str)
+    {
+      // a better solution is possible like this:
+      // return str.ToLower().GroupBy(c => c).Where(g => g.Count() > 1).Count();
+      var lowerCaseStr = str.ToLower();
+      return lowerCaseStr
+        .Distinct()
+        .Where(distinctChar => lowerCaseStr.Count(currChar => currChar == distinctChar) > 1)
+        .Count();
+    }
+  }
+}
